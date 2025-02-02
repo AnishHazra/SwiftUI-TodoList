7c42f5e5d26bf5a771589eef2cbb23f94fa74a56
AnishHazra <anishhazra108@gmail.com>
2025-02-02 10:48:22 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Anish Hazra on 02/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
